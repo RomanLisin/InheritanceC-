945c509c8071b1cf7616db4b676737538a14c486
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-09-01 23:45:29 +1000

    Add project files.

---
+++ Academy/Program.cs
@@ -0,0 +1,73 @@
+﻿//#define INHERITANCE_1
+//#define INHERITANCE_2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Program
+	{
+		static readonly string delimetr = "\n-----------------------------------------------\n";
+		static void Main(string[] args)
+		{
+#if INHERITANCE_1
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Print();
+			Console.WriteLine(human);
+			Console.WriteLine(delimetr);
+
+			Student student = new Student("Pinkman", "Jessie", 25, "Chemistry", "WW_220", 95, 97);
+			student.Print();
+			Console.WriteLine(student);
+			Console.WriteLine(delimetr);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Print();
+			Console.WriteLine(teacher);
+			Console.WriteLine(delimetr);
+
+			Graduate graduate = new Graduate("Vasya", "Ivanov", 28, "Chemistry", "WW_221", 98, 92, "Mathematic");
+			graduate.Print();
+			Console.WriteLine(graduate);
+			Console.WriteLine(delimetr); 
+#endif
+#if INHERITANCE_2
+			Human tommy = new Human("Vercetty", "Tommy", 30);
+			Console.WriteLine(tommy);
+
+			Human ken = new Human("Rozenberg", "Ken", 35);
+			Console.WriteLine(ken);
+
+			Human diaz = new Human("Diaz", "Ricardo", 50);
+			Console.WriteLine(diaz);
+
+
+			Student s_tommy = new Student(tommy, "Theft", "Vice", 98, 99);
+			Console.WriteLine(s_tommy);
+
+			Student s_ken = new Student(ken, "Lawer", "Vice", 42, 35);
+
+			Graduate g_tommy = new Graduate(s_tommy, "How to make money");
+			Console.WriteLine(g_tommy);
+
+			Teacher t_diaz = new Teacher(diaz, "Weapons distribution", 20);
+			Console.WriteLine(t_diaz);
+
+#endif
+			//Generalization:
+			Human[] group = new Human[]
+			{
+				new Student("Pinkman", "Jessie", 25, "Chemistry", "WW_220", 95, 97),
+				new Teacher("White", "Walter", 50, "Chemistry", 25),
+				new Graduate("Vasya", "Ivanov", 28, "Chemistry", "WW_221", 98, 92, "Mathematic")
+			};
+			for (int i=0; i<group.Length; i++)
+			{
+				Console.WriteLine(group[i]);
+            }
+		}
+	}
+}
